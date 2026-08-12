9f67abf950b2229f73c2e63e2e203085aaffb34a
Niraj Kumar <nirajkr2707@gmail.com>
2026-08-12 13:01:36 +0530

    Search two pointer

---
+++ Searching/LinearSearch.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class LinearSearch {
+
+    static int search(int[] arr, int num) {
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == num) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int num = sc.nextInt();
+
+        int result = search(arr, num);
+
+        System.out.println(result);
+
+        sc.close();
+    }
+}
